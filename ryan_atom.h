0f81ea1ec73d8720f26daabf34a49b1a99f1cf9f
Ryan Seys <ryan@ryanseys.com>
2014-11-30 16:26:38 -0500

    Nitrogen is blue

--- ryan_atom.h
+++ ryan_atom.h
@@ -34,6 +34,12 @@ public:
         this->sphere->setDiffuse(1.0, 0.15, 0.15); // material diffuse color
         this->sphere->setSpecular(1.0, 0.15, 0.15); // material specular color
         break;
+      case 'N':
+        // nitrogen is blue
+        this->sphere->setAmbient(0.1, 0.1, 1.0); // material ambient color
+        this->sphere->setDiffuse(0.1, 0.1, 1.0); // material diffuse color
+        this->sphere->setSpecular(0.1, 0.1, 1.0); // material specular color
+        break;
       default:
         this->sphere->setAmbient(1.0, 0.0, 0.2); // material ambient color
         this->sphere->setDiffuse(1.0, 1.0, 0.5); // material diffuse color
